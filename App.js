4458175c44fb4bd297c7998dc35e73ccbeec0454
Nischal <dhungeln12@gmail.com>
2024-02-11 17:41:31 +0545

    learn alert and statusbar component

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import { useState } from "react";
-import { Text, View, Modal, Button } from "react-native";
+import {
+  Text,
+  View,
+  Modal,
+  Button,
+  StatusBar,
+  ActivityIndicator,
+  Alert,
+} from "react-native";
 export default function App() {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isStatusBarHidden, setIsStatusBarHidden] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button
+      {/* {//! 1. Modal} */}
+
+      {/* <Button
         title="Open Modal"
         color="midnightblue"
         onPress={() => {
           setIsModalVisible(true);
         }}
-      />
-
+      /> */}
+      {/* 
       <Modal
         visible={isModalVisible}
         onRequestClose={() => {
@@ -30,7 +41,65 @@ export default function App() {
             }}
           />
         </View>
-      </Modal>
+      </Modal> */}
+
+      {/* {//! 2. Status bar} */}
+
+      <StatusBar
+        backgroundColor="lightgreen"
+        barStyle="light-content"
+        hidden={isStatusBarHidden}
+      />
+      {/* {console.log(isStatusBarHidden)} */}
+      <Button
+        title={isStatusBarHidden ? "Show status bar" : "Hide status bar"}
+        onPress={() => {
+          if (isStatusBarHidden) {
+            setIsStatusBarHidden(false);
+          } else {
+            setIsStatusBarHidden(true);
+          }
+        }}
+      />
+
+      {/* { //! Activity indicator} */}
+      {/* 
+      <ActivityIndicator />
+      <ActivityIndicator color="midnightblue " />
+      <ActivityIndicator size="large" color="darkblue" />
+      <ActivityIndicator
+        size="large"
+        color="red"
+        animating={isStatusBarHidden}
+      /> */}
+
+      {/* {//! Alert} */}
+
+      <Button
+        title="Alert"
+        onPress={() => {
+          Alert.alert("Invalid data!", "DOB Incorrect");
+        }}
+      />
+      <Button
+        title="Alert 2"
+        onPress={() => {
+          Alert.alert("Invalid data!", "DOB Incorrect", [
+            {
+              text: "Cancel",
+              onPress: () => {
+                console.log("Cancel Pressed");
+              },
+            },
+            {
+              text: "OK",
+              onPress: () => {
+                console.log("OK Pressed");
+              },
+            },
+          ]);
+        }}
+      />
     </View>
   );
 }
